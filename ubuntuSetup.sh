60fc5c1c84d5157abe4e39af2742b4b5edaeba74
Aaron Niskin <amniskin@gmail.com>
2018-07-14 16:19:33 -0700

    updating ubuntuSetup.sh

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -8,6 +8,40 @@ then
 else
 	echo "$tmpDir already exists. Using old directory"
 fi
+
+errorFile=$tmpDir/errors.txt
+logFile="$tmpDir/log.txt"
+
+if [ ! -f $logFile ]; then
+	echo "Started script at $(date)" > $logFile
+	echo "----------------------------------------"
+else
+	for i in {1..4}; do
+		for j in {1..50}; do
+			echo -n "="
+		done
+	done
+	echo "Restarted script at $(date)" > $logFile
+	echo "----------------------------------------"
+fi
+
+echo "Making directories..."
+
+oldDir="$HOME/old"
+dirs=("$oldDir" "$HOME/.r" "$HOME/.config" "$HOME/.config/r" "$HOME/.vim-tmp")
+for dir in "${dirs[@]}"; do
+	if [ ! -d $dir ]; then
+		mkdir $dir && echo "Made directory at $dir" >> $logFile
+	else
+		echo "$dir already exists. Using old directory" >> $logFile
+	fi
+done
+
+
+echo Setting up ssh keys...
+ssh-keygen -t rsa
+
+
 echo "updating repos..."
 sudo -H apt-get update
 echo "copying /etc/apt/sources.list to $tmpDir/sources.list"
@@ -15,15 +49,13 @@ cp /etc/apt/sources.list $tmpDir/
 ## echo "adding canonical parter repos..."
 ## cat /etc/apt/sources.list | sed -r 's/\#\ deb\ (.*)partner/deb\ \1partner/' > /etc/apt/sources.list
 
-echo "" > $tmpDir/errors.txt
-
 ## apt-get packages... Left out: "mongodb" "postgresql" 
-packages=("vim" "git" "i3" "xbacklight" "python-pip" "curl" "ruby" "ruby-dev" "jekyll" "tree" "chromium-browser" "firefox" "gnupg" "vlc" "compton" "adobe-flashplugin" "texlive-full" "ninvaders" "gcc" "g++" "feh" "gimp" "xclip" "transmission" "r-base" "pandoc" "openjdk-8-jdk" "openjdk-8-jre" "default-jre" "default-jdk" "tmux")
+packages=("vim" "git" "i3" "xbacklight" "python-pip" "curl" "ruby" "ruby-dev" "jekyll" "tree" "chromium-browser" "firefox" "gnupg" "vlc" "compton" "adobe-flashplugin" "texlive-full" "ninvaders" "gcc" "g++" "feh" "gimp" "xclip" "transmission" "r-base" "pandoc" "openjdk-8-jdk" "openjdk-8-jre" "default-jre" "default-jdk" "openjdk-9-jdk" "tmux")
 
 for package in "${packages[@]}"
 do
 	echo "installing $package..." &&
-		sudo apt-get install -yq $package || echo "apt-get install error ==> $package\n" >> $tmpDir/errors.txt
+		sudo apt-get install -yq $package || echo "apt-get install error ==> $package\n" >> $logFile
 done
 
 echo "================================================="
@@ -41,29 +73,27 @@ git clone https://github.com/amniskin/.dotfiles.git ~/.dotfiles
 files=(".vimrc" ".bashrc" ".bash_aliases" ".mrjob.conf" ".i3/config"
 ".i3/i3status.conf")
 
-if [ ! -d ~/.i3 ]
-then
-	mkdir ~/.i3
-fi
-
-for file in "${files[@]}"
-do
-	if [ -f ~/$file ]
-	then
-		rm ~/$file
+for from in $(find $HOME/.dotfiles/home); do
+	to=$(echo $from | sed 's/\/.dotfiles\/home//')
+	if [ -f $from ]; then
+		if [ -f $to ]; then
+			tmp=$toDir/$(echo $to | tr "/" "+")
+			mv $to $oldDir/$tmp && echo "moved $to to $oldDir/$tmp" >> $logFile
+		fi
+		echo "Linking $from $to"
+		ln -s $from $to || echo "error creating symbolic link from $from to $to" >> $logFile
+	else
+		echo "skipping $from"
 	fi
-	ln -s ~/.dotfiles/home/$file ~/$file ||
-		echo "symlink error ==> $file\n" >> $tmpDir/errors.txt
 done
 
-
-sudo bash -c "cd /usr/local/bin && curl -fsSLo boot https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh && chmod 755 boot"
-
-mkdir ~/.vim-tmp
 echo "installing VimVundle... "
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "installing Vim Pluggins... "
 vim +PluginInstall +qall
+
+echo "===================   Boot   ===================="
+sudo bash -c "cd /usr/local/bin && curl -fsSLo boot https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh && chmod 755 boot"
 
 echo "================================================="
 echo "================================================="
@@ -72,14 +102,19 @@ echo "================================================="
 echo "================================================="
 
 ##  pip packages
-packages=("py3status" "numpy" "pandas" "matplotlib" "scikit-learn" "statsmodels" "pandas-datareader" "yahoo-finance" "wikipedia" "gensim" "beautifulsoup4" "scipy" "pymongo" "mrjob" "beautifulsoup4" "powerline-status")
+packages=("jupyter" "jupyter_contrib_nbextensions" "py3status" "numpy" "pandas" "matplotlib" "scikit-learn" "statsmodels" "pandas-datareader" "yahoo-finance" "wikipedia" "gensim" "beautifulsoup4" "scipy" "pymongo" "mrjob" "beautifulsoup4" "powerline-status" "cython")
 
 sudo pip install --upgrade pip &&
 	for package in "${packages[@]}"
 	do
 		sudo -H pip install $package ||
-			echo "pip install error ==> $package\n" >> $tmpDir/errors.txt
+			echo "pip install error ==> $package\n" >> $logFile
 	done
+
+jupyter contrib nbextension install --user
+
+sudo -H pip2 install ipykernel &&
+	python2 -m ipykernel install --user
 
 ##  echo "================================================="
 ##  echo "================================================="
@@ -119,12 +154,24 @@ echo "===================  Ruby  ======================"
 echo "================================================="
 echo "================================================="
 
-## ruby gems
-packages=("bundler")
+packages=("bundler" "jekyll")
 for package in "${packages[@]}"
 do
 	gem install $package ||
-		echo "gem install error ==> $package\n" >> $tmpDir/errors.txt
+		echo "gem install error ==> $package\n" >> $logFile
+done
+
+echo "================================================="
+echo "================================================="
+echo "=====================  R  ======================="
+echo "================================================="
+echo "================================================="
+
+packages=("dplyr" "tidyverse" "rmarkdown")
+for package in "${packages[@]}"
+do
+	R -e "install.packages(\"$package\", lib=\"$HOME/.r\")" ||
+		echo "R install error ==> $package" >> $logFile
 done
 
 echo "Y'all have a good day now, y'hear?"
