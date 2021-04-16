f88c92451cdd79fd7b73e7deaf5c35e431ad711c
Aaron Niskin <amniskin@gmail.com>
2021-04-15 20:09:28 -0500

    fixed buggies

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -46,12 +46,6 @@ sudo apt-get update
 
 sudo apt-get install vim git python3-dev python3-pip python3-virtualenv curl jekyll tree gnupg ninvaders gcc g++ pandoc default-jre default-jdk tmux suckless-tools ctags
 
-if [ -z ${noinstall+x} ]; then
-    echo "skipping interactive stuff..."
-else
-    sudo apt-get i3 xbacklight jekyll chromium-browser firefox compton feh transmission ruby ruby-dev gimp xclip
-fi
-
 git config --global user.email "amniskin@gmail.com"&&
 git config --global user.name "Aaron Niskin"
 
@@ -63,30 +57,30 @@ echo "===================   Boot   ===================="
 sudo bash -c "cd /usr/local/bin && curl -fsSLo boot https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh && chmod 755 boot"
 mkdir $HOME/.boot $HOME/.m2
 
-echo "================================================="
-echo "================================================="
-echo "===================  Ruby  ======================"
-echo "================================================="
-echo "================================================="
-
-packages=("bundler" "jekyll")
-for package in "${packages[@]}"
-do
-    gem install $package ||
-        echo "gem install error ==> $package\n" >> $logFile
-done
+if [ -z ${noinstall+x} ]; then
+    echo "skipping interactive stuff..."
+else
+    sudo apt-get i3 xbacklight jekyll chromium-browser firefox compton feh transmission ruby ruby-dev gimp xclip
+    cat <<EOT | bash
+    cd $tmpdir &&
+        git clone git@github.com:Boruch-Baum/morc_menu.git &&
+        cd morc_menu &&
+        sudo make install
+EOT
+    packages=("bundler" "jekyll")
+    for package in "${packages[@]}"
+    do
+        gem install $package ||
+            echo "gem install error ==> $package\n" >> $logFile
+    done
+fi
 
 echo "installing VimVundle... "
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "installing Vim Pluggins... "
 vim +PluginInstall +qall
 
-mkdir -p $HOME/.misc_things/ &&
-    cd $HOME/.misc_things &&
-    git clone git@github.com:Boruch-Baum/morc_menu.git &&
-    cd morc_menu &&
-    sudo make install
-cd $HOME/.misc_things/ &&
+cd $tmpdir &&
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" &&
     unzip awscliv2.zip &&
     sudo ./aws/install
