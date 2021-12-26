3c10dfbe09a59ff277a5ae91645c84ae026bd4cf
Aaron Niskin <amniskin@gmail.com>
2021-12-25 22:49:49 -0600

    brightness tweak

--- bin/brightness.sh
+++ bin/brightness.sh
@@ -4,43 +4,35 @@ max=$(cat /sys/class/backlight/intel_backlight/max_brightness)
 current=$(cat /sys/class/backlight/intel_backlight/brightness)
 current_pct=$(printf %.0f $(echo "scale=3; $current * 100 / $max" | bc))
 
-upper() {
-  if [ $current_pct -lt 0 ]; then
-    new_pct=0
-  elif [ $current_pct -lt 10 ]; then
-    new_pct=$(($current_pct + 1))
-  elif [ $current_pct -lt 20 ]; then
-    new_pct=$(($current_pct + 2))
-  elif [ $current_pct -lt 50 ]; then
-    new_pct=$(($current_pct + 5))
-	elif [ $current_pct -lt 90 ]; then
-    new_pct=$(($current_pct + 10))
-  else
-		new_pct=100
-  fi
-	echo $new_pct
-}
-
-downer() {
-  if [ $current_pct -le 0 ]; then
-    new_pct=0
-  elif [ $current_pct -le 10 ]; then
-    new_pct=$(($current_pct - 1))
-  elif [ $current_pct -le 20 ]; then
-    new_pct=$(($current_pct - 2))
-  elif [ $current_pct -le 50 ]; then
-    new_pct=$(($current_pct - 5))
-	elif [ $current_pct -le 90 ]; then
-    new_pct=$(($current_pct - 10))
-  else
-		new_pct=90
-  fi
-	echo $new_pct
-}
-
 case $1 in
-  +) new_pct=$(upper $current_pct) ;;
-  -) new_pct=$(downer $current_pct) ;;
+  +)
+    if [ $current_pct -lt 0 ]; then
+      new_pct=0
+    elif [ $current_pct -lt 10 ]; then
+      new_pct=$(($current_pct + 1))
+    elif [ $current_pct -lt 20 ]; then
+      new_pct=$(($current_pct + 2))
+    elif [ $current_pct -lt 50 ]; then
+      new_pct=$(($current_pct + 5))
+    elif [ $current_pct -lt 90 ]; then
+      new_pct=$(($current_pct + 10))
+    else
+      new_pct=100
+    fi ;;
+  -)
+    if [ $current_pct -le 0 ]; then
+      new_pct=0
+    elif [ $current_pct -le 10 ]; then
+      new_pct=$(($current_pct - 1))
+    elif [ $current_pct -le 20 ]; then
+      new_pct=$(($current_pct - 2))
+    elif [ $current_pct -le 50 ]; then
+      new_pct=$(($current_pct - 5))
+    elif [ $current_pct -le 90 ]; then
+      new_pct=$(($current_pct - 10))
+    else
+      new_pct=90
+    fi ;;
   *) echo $current_pct && exit 0;;
 esac
 
